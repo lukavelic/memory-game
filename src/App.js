bdb2f28413423f644a211f735cc45bcb0dbae136
lukavelic <96191586+lukavelic@users.noreply.github.com>
2023-01-10 17:03:39 +0100

    Implement score

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import './App.css';
 import Header from './layouts/Header';
 import MemoryCard from './components/MemoryCard';
@@ -31,32 +31,86 @@ function App() {
     }
   );
 
+  const [score, setScore] = useState({
+    current: 0,
+    high: 0,
+  });
+
+  useEffect(() => {
+    const shuffle = array => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      };
+    };
+
+    return () => {
+      console.log('use effect')
+      shuffle(childIds);
+    }
+  },[memoryCards])
+
   const root = memoryCards[0];
   const childIds = root.childIds;
 
   const clickHandler = (e) => {
     const id = parseInt(e.target.id);
 
-    setMemoryCards({
-      ...memoryCards,
-      [id]: {
-        id: memoryCards[id].id,
-        img: memoryCards[id].img,
-        clicked: true,
+    if(memoryCards[id].clicked === true) {
+      restartGame();    
+    } else {
+      setMemoryCards({
+        ...memoryCards,
+        [id]: {
+          id: memoryCards[id].id,
+          img: memoryCards[id].img,
+          clicked: true,
+        }
+      });
+
+      incrementScore();
+    }
+    console.log(memoryCards)
+    console.log(score)
+  };
+
+  const restartGame = () => {
+    setMemoryCards(
+      {
+        0: {
+          id: 0,
+          childIds: [1,2]
+        },
+        1: {
+          id: 1,
+          img: 'placeholder',
+          clicked: false,
+        },
+        2: {
+          id: 2,
+          img: 'another placeholder',
+          clicked: false,
+        }
       }
-    });
+    );
 
-    shuffle(childIds);
-  };
+    setScore({
+      current: 0,
+      high: score.current,
+    })
+  }
 
-  const shuffle = array => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    };
-  };
+  const incrementScore = () => {
+    setScore({
+      current: score.current + 1,
+      high: score.high,
+    })
+  }
+  
+
+
   
   return (
     <div>
